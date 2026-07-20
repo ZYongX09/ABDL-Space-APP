cd69c9c615fa75bd6b07a163b04b5ebda71ddfbf
ZYongX09 <3806526113@qq.com>
2026-07-20 10:23:36 +0800

    feat(ui): 扩充心理危机检测词库

--- mastodon/src/main/java/org/joinmastodon/android/fragments/ComposeFragment.java
+++ mastodon/src/main/java/org/joinmastodon/android/fragments/ComposeFragment.java
@@ -195,7 +195,7 @@ public class ComposeFragment extends MastodonToolbarFragment implements ComposeE
 	private View crisisCard;
 	private CrisisWarningViewController crisisWarningController;
 	private boolean crisisWarningDismissed;
-	private static final String[] CRISIS_KEYWORDS={"自杀", "自残", "死亡", "抑郁", "双向", "双相", "ADHD", "PTSD", "童年创伤", "不想活", "不想活了", "不想再活", "活不下去", "活着没意思", "活着没有意义", "想死", "去死", "求死", "寻死", "轻生", "结束生命", "结束自己的生命", "了结自己", "离开这个世界", "永远消失", "绝望", "割腕", "割脉", "跳楼", "跳河", "吞药", "自我伤害", "伤害自己", "伤害我自己"};
+	private static final String[] CRISIS_KEYWORDS={"自杀", "自残", "死亡", "抑郁", "双向", "双相", "ADHD", "PTSD", "童年创伤", "不想活", "不想活了", "不想再活", "活不下去", "活着没意思", "活着没有意义", "想死", "去死", "求死", "寻死", "轻生", "结束生命", "结束自己的生命", "了结自己", "离开这个世界", "永远消失", "绝望", "没必要活", "没有活下去的理由", "不值得活", "死了算了", "一死了之", "结束一切", "解脱了", "撑不下去了", "不想醒来", "希望永远睡过去", "割腕", "割脉", "拿刀割自己", "划伤自己", "跳楼", "跳河", "撞车", "卧轨", "吞药", "服毒", "上吊", "自我伤害", "伤害自己", "伤害我自己"};
 	private int newBabyWorldBindingState;
 	private int newBabyWorldBindingRequestGeneration;
 	private int aiRecommendationRequestGeneration;
